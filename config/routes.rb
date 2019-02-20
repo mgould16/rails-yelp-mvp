c3efd21f75f39f950dd14e6386954b2d54a1d643
Mark Gould <m.gould16@gmail.com>
2019-02-20 15:20:00 +0000

    completion before iteration

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Read all tasks
-  get 'restaurants', to: 'restaurants#index'
+  # # Read all tasks
+  # get 'restaurants', to: 'restaurants#index'
 
-  # Read the details of one restaurant
-  get 'restaurant/:id', to: 'restaurants#show', as: "restaurant"
+  # # Read the details of one restaurant
+  # get 'restaurant/:id', to: 'restaurants#show', as: "restaurant"
 
-    # create a restaurant 2 REQUESTS
-  get 'restaurants/new', to: 'restaurants#new', as: "new"
-  post 'restaurants', to: "restaurants#create"
+  #   # create a restaurant 2 REQUESTS
+  # get 'restaurants/new', to: 'restaurants#new', as: "new"
+  # post 'restaurants', to: "restaurants#create"
 
-  # update a restaurant
-  get 'restaurant/:id/edit', to: 'restaurants#edit', as: "edit"
-  patch 'restaurant/:id', to: 'restaurants#update'
+  # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: "new_restaurant_review"
+  # post 'reviews', to: "reviews#create"
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+
+
+  # resources :reviews, only: [ :show, :edit, :update, :destroy ]
 
 end
